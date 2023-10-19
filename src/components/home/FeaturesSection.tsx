292d4e7b5dddadbcf00626c52e9f78a14f82842b
Luis Dourado <lfaddourado@gmail.com>
2023-10-19 13:26:40 -0300

    fix: comment still not ready parts

--- src/components/home/FeaturesSection.tsx
+++ src/components/home/FeaturesSection.tsx
@@ -16,10 +16,10 @@ export default function FeaturesSection() {
       <FeaturesSteps />
       <div className="mx-auto max-w-6xl px-6 lg:px-8">
         <div className="mt-16 flow-root sm:mt-24">
-          <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+          {/* <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
             placeholder img w1354 h866 qual100 cl='rounded-md bg-white p-2
             sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
