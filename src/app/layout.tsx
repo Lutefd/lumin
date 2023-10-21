5a742a1a37c9c439d9b90ab9f44c53acb2f4f3d3
Luis Dourado <lfaddourado@gmail.com>
2023-10-20 22:06:09 -0300

    feat: add toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { headers } from "next/headers";
 import { TRPCReactProvider } from "@/trpc/react";
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/Navbar";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -31,6 +32,7 @@ export default function RootLayout({
         )}
       >
         <TRPCReactProvider headers={headers()}>
+          <Toaster />
           <Navbar />
           {children}
         </TRPCReactProvider>
