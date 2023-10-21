775037d524930be27cd30c89c32fe9f5ff05d671
Luis Dourado <lfaddourado@gmail.com>
2023-10-20 22:07:29 -0300

    feat: add and personilize ui btns

--- src/components/dashboard/UploadButton.tsx
+++ src/components/dashboard/UploadButton.tsx
@@ -3,6 +3,13 @@
 import { useState } from "react";
 import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
 import { Button } from "../ui/button";
+import Dropzone from "react-dropzone";
+import { Cloud, File, Loader2 } from "lucide-react";
+import { Progress } from "../ui/progress";
+import { useUploadThing } from "@/lib/uploadthing";
+import { useToast } from "../ui/use-toast";
+import { api } from "@/trpc/react";
+import { useRouter } from "next/navigation";
 
 export default function UploadButton() {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,7 +25,128 @@ export default function UploadButton() {
       <DialogTrigger asChild onClick={() => setIsOpen(true)}>
         <Button>Fazer Upload</Button>
       </DialogTrigger>
-      <DialogContent>example</DialogContent>
+      <DialogContent>
+        <UploadDropzone />
+      </DialogContent>
     </Dialog>
   );
 }
+
+const UploadDropzone = () => {
+  const router = useRouter();
+  const [isUploading, setIsUploading] = useState<boolean | null>(true);
+  const [uploadProgress, setUploadProgress] = useState<number>(0);
+
+  const { startUpload } = useUploadThing("pdfUploader");
+  const { mutate: startPolling } = api.files.validateFile.useMutation({
+    onSuccess: (file) => {
+      router.push(`/dashboard/${file.id}`);
+    },
+    retry: true,
+    retryDelay: 500,
+  });
+  const { toast } = useToast();
+
+  const startSimulatedProgress = () => {
+    setUploadProgress(0);
+
+    const interval = setInterval(() => {
+      setUploadProgress((prevProgress) => {
+        if (prevProgress >= 95) {
+          clearInterval(interval);
+          return prevProgress;
+        }
+        return prevProgress + 5;
+      });
+    }, 500);
+    return interval;
+  };
+
+  return (
+    <Dropzone
+      multiple={false}
+      onDrop={async (acceptedFile) => {
+        setIsUploading(true);
+        const progressInterval = startSimulatedProgress();
+        const res = await startUpload(acceptedFile);
+        console.log("Files dropped:", acceptedFile);
+
+        if (!res) {
+          return toast({
+            title: "Algo deu errado",
+            description: "Por favor tente novamente",
+            variant: "destructive",
+          });
+        }
+
+        const [fileResponse] = res;
+
+        const key = fileResponse?.key;
+
+        if (!key) {
+          return toast({
+            title: "Algo deu errado",
+            description: "Por favor tente novamente",
+            variant: "destructive",
+          });
+        }
+
+        clearInterval(progressInterval);
+        setUploadProgress(100);
+
+        startPolling({ key });
+      }}
+    >
+      {({ getRootProps, getInputProps, acceptedFiles }) => (
+        <div
+          {...getRootProps()}
+          className="m-4 h-64 rounded-lg border border-dashed border-gray-300"
+        >
+          <div className="flex h-full w-full items-center justify-center">
+            <label
+              htmlFor="dropzone-file"
+              className="flex h-full w-full cursor-pointer flex-col items-center justify-center rounded-lg bg-gray-50 hover:bg-gray-100"
+              onClick={(e) => {
+                e.preventDefault();
+              }}
+            >
+              <div className="flex flex-col items-center justify-center pb-6 pt-5">
+                <Cloud className="mb-2 h-6 w-6 text-zinc-500" />
+                <p className="mb-2 text-sm text-zinc-700">
+                  <span className="font-semibold">Arraste seu arquivo</span> ou
+                  clique para fazer o upload
+                </p>
+                <div className="text-xs text-zinc-500">PDF (até 4mb)</div>
+              </div>
+              {acceptedFiles?.[0] ? (
+                <div className="flex max-w-xs items-center divide-x divide-zinc-200 overflow-hidden rounded-md bg-white outline outline-[1px] outline-zinc-200">
+                  <div className="grid h-full place-items-center px-3 py-2">
+                    <File className="h-4 w-4 text-primary" />
+                  </div>
+                  <div className="h-full truncate px-3 py-2 text-sm">
+                    {acceptedFiles[0].name}
+                  </div>
+                </div>
+              ) : null}
+              {isUploading ? (
+                <div className="mx-auto mt-4 w-full max-w-xs">
+                  <Progress
+                    value={uploadProgress}
+                    className="h-1 w-full bg-zinc-200"
+                  />
+                  {uploadProgress === 100 ? (
+                    <div className="flex items-center justify-center gap-1 pt-2 text-center text-sm text-zinc-700">
+                      <Loader2 className="h-3 w-3 animate-spin" />
+                      Redirecionando...
+                    </div>
+                  ) : null}
+                </div>
+              ) : null}
+            </label>
+            <input {...getInputProps()} id="dropzone-file" className="hidden" />
+          </div>
+        </div>
+      )}
+    </Dropzone>
+  );
+};
