caa42b6743490247b05edcc3b57b26d6684f0de1
Luis Dourado <lfaddourado@gmail.com>
2023-10-20 22:05:53 -0300

    feat: setup configs for file upload and rendering

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
@@ -5,6 +7,12 @@
 await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const configuration = {
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
+  },
+};
 
-export default config;
+export default configuration;
