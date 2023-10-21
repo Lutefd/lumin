2a4a3dec97d623572f574615c3cf1ab2c62166b7
Luis Dourado <lfaddourado@gmail.com>
2023-10-20 22:06:47 -0300

    feat: setup uploadthing

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,37 @@
+import { getServerAuthSession } from "@/server/auth";
+import { db } from "@/server/db";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+const f = createUploadthing();
+
+// FileRouter for your app, can contain multiple FileRoutes
+export const ourFileRouter = {
+  // Define as many FileRoutes as you like, each with a unique routeSlug
+  pdfUploader: f({ pdf: { maxFileSize: "16MB" } })
+    // Set permissions and file types for this FileRoute
+    .middleware(async () => {
+      // This code runs on your server before upload
+      const session = await getServerAuthSession();
+
+      const user = session?.user;
+
+      // If you throw, the user will not be able to upload
+      if (!user) throw new Error("Unauthorized");
+
+      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      return { userId: user.id, userEmail: user.email };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      const createdFIle = await db.file.create({
+        data: {
+          key: file.key,
+          name: file.name,
+          userId: metadata.userId,
+          url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+          uploadStatus: "PROCESSING",
+        },
+      });
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
---
+++ src/lib/uploadthing.ts
@@ -0,0 +1,6 @@
+import { generateReactHelpers } from "@uploadthing/react/hooks";
+import { UTApi } from "uploadthing/server";
+import type { OurFileRouter } from "@/app/api/uploadthing/core";
+
+export const utapi = new UTApi();
+export const { useUploadThing } = generateReactHelpers<OurFileRouter>();
